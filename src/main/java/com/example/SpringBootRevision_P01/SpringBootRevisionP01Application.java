4507d69c9542ac7e1922fd52cb44c7a27d5fa6de
parasbramhankar <parasbramhankar.in@gmail.com>
2026-01-16 13:20:09 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBootRevision_P01/SpringBootRevisionP01Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRevision_P01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRevisionP01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRevisionP01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootRevision_P01/SpringBootRevisionP01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRevision_P01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRevisionP01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
