97c3542fd0b87d49747c1a310d49a8fa49f46ebe
parasbramhankar <parasbramhankar.in@gmail.com>
2026-01-16 13:40:12 +0530

    commit

---
+++ src/main/java/com/example/SpringBootRevision_P01/entity/Student.java
@@ -0,0 +1,17 @@
+package com.example.SpringBootRevision_P01.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+    private int age;
+}
